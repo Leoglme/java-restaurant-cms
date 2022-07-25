3f50a1fde678dd8d38f8ec201a380e6148604f9e
Léo Guillaume <leo.guillaume@epitech.eu>
2022-07-25 16:45:00 +0200

    render view

--- src/main/java/com/JavaRestaurantCmsApplication.java
+++ src/main/java/com/JavaRestaurantCmsApplication.java
@@ -1,20 +1,11 @@
 package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class JavaRestaurantCmsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaRestaurantCmsApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
 	}
 }
 
