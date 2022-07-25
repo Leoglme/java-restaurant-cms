0d8366d781380338579bfd9c844b07a67ef65e08
Léo Guillaume <leo.guillaume@epitech.eu>
2022-07-25 11:39:55 +0200

    init spring project

---
+++ src/test/java/com/example/javarestaurantcms/JavaRestaurantCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javarestaurantcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestaurantCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
