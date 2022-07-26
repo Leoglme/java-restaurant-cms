735de8b9b6586ef3c9e1a5dd13d6df81f8fc260f
Léo Guillaume <leo.guillaume@epitech.eu>
2022-07-26 17:00:17 +0200

    init npm and tailwindcss for views

--- src/main/java/com/javarestaurantcms/JavaRestaurantCmsApplication.java
+++ src/main/java/com/javarestaurantcms/JavaRestaurantCmsApplication.java
@@ -2,23 +2,12 @@ package com.javarestaurantcms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
 public class JavaRestaurantCmsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaRestaurantCmsApplication.class, args);
-	}
-
-	@GetMapping
-	public String showHome(Model model) {
-		return "MesgPage";
-	}
-	@GetMapping("/toto")
-	public String showToto(Model model) {
-		return "toto";
 	}
 }
 
